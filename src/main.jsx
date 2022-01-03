6cfa5d5a497731d7c3f454ee4c823c76f5d2aa96
ManiShankarGajavelli <gmanishanker@gmail.com>
2022-01-03 12:01:14 +0530

    Adding no Match Route.

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,14 @@ ReactDOM.render(
       <Route path="/" element={<App />}>
         <Route path="/invoices" element={<Invoices />} />
         <Route path="/expenses" element={<Expenses />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Route>
     </Routes>
   </Router>,
