14a9830495895e8cbe832d77fef6d2b2fd3a42f1
ManiShankarGajavelli <gmanishanker@gmail.com>
2022-01-03 12:37:05 +0530

    1 file modified

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { getInvoice } from '../data';
 export default function Invoice() {
   let params = useParams();
-  let invoice = getInvoice(params.InvoiceId, 10);
+  let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
     <main style={{ padding: '1rem' }}>
       <h2>Total Due: {invoice.amount}</h2>
