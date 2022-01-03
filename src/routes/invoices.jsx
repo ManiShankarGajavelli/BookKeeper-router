3284ea865f91368d715dfc31ea04e3ea69fc3dea
ManiShankarGajavelli <gmanishanker@gmail.com>
2022-01-03 11:46:29 +0530

    1 file modified

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -2,6 +2,7 @@ import Link from 'react-router-dom';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
+  let invoices = getInvoices();
   return (
     <div style={{ display: 'flex' }}>
       <nav
